40507337bcb5d07f0d9929ea71ac74eae37e35ae
Mateusz <mateusz.adamowicz.lo1@gmail.com>
2024-10-09 13:37:39 +0200

    Initial Commit

---
+++ src/Exercise_3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercise_3 {
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+        int myNumber = inputScanner.nextInt();
+        printTable(myNumber);
+    }
+
+    public static void printTable(int number) {
+        for(int i=1; i<11; i++) {
+            System.out.println(number * i);
+        }
+    }
+}
